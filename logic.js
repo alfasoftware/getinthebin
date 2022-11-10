e30d8c4597de01cd73efa4cacb41dd1d9acdae7f
Oliver Brownlow <44783188+olliebrownlow@users.noreply.github.com>
2022-11-10 12:21:43 +0000

    add initial score logic, update bin images and styling
    Co-Authored-By: Ruth Baker <33294286+ruthmoog@users.noreply.github.com>
    Co-Authored-By: Will Howes <48259950+willhowes@users.noreply.github.com>
    Co-Authored-By: gretaabel <117906972+gretaabel@users.noreply.github.com>

--- logic.js
+++ logic.js
@@ -1,16 +1,26 @@
+// drag and drop functions
 function allowDrop(event) {
-    event.preventDefault();
-  }
+  event.preventDefault();
+}
 
 function drag(event) {
-    console.log("drag event");
-    event.dataTransfer.setData("target", event.target.id);
-  }
+  console.log("drag event");
+  event.dataTransfer.setData("target", event.target.id);
+}
 
 function drop(event) {
-    console.log("drop event");
-    console.log(event.target);
-    event.preventDefault();
-    var data = event.dataTransfer.getData("target");
-    event.target.appendChild(document.getElementById(data));
-  }
+  console.log("drop event");
+  console.log(event.target);
+  event.preventDefault();
+  var data = event.dataTransfer.getData("target");
+  event.target.appendChild(document.getElementById(data));
+  getScore();
+}
+
+// score logic
+var score = 0;
+
+function getScore() {
+  document.getElementById("score").innerHTML = score;
+}
+
